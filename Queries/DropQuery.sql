3a0071dd3e312501a5597366d767fa78a673ed5a
Nonika Reingold <veronika.reingold@gmail.com>
2023-04-05 18:31:11 -0400

    Added Triggers into the DropQuery

--- Queries/DropQuery.sql
+++ Queries/DropQuery.sql
@@ -1,10 +1,21 @@
 /*
-	Author: Shieanne Bennett
+	Author: Shieanne Bennett and Nonika Reingold
 	COMP-3150 Final Project Submission
 	Drop Queries for Hospital Database, provided
 	to verify no duplicate data exists in the
 	database. Run before EMRSCHEMA.sql
 */
+
+-- Drop Triggers so you can create them
+-- if they are already present
+DROP TRIGGER TR_Admit_Insert;
+DROP TRIGGER TR_Patient_Insert;
+DROP TRIGGER TR_Doctor_Insert;
+DROP TRIGGER TR_Nurse_Insert;
+DROP TRIGGER TR_Hospital_Insert;
+DROP TRIGGER TR_Pharmacy_Insert;
+DROP TRIGGER TR_Location_Insert;
+GO
 
 -- Drop relationship tables first since they contain
 -- foreign keys and will prevent entity tables from
@@ -45,3 +56,5 @@ GO
 -- if it's already present
 DROP SCHEMA EMR;
 GO
+
+
