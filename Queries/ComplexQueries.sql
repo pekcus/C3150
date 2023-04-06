9d1dba369999f74589284665eb13ef8b2f0edd45
Nonika Reingold <veronika.reingold@gmail.com>
2023-04-06 16:31:24 -0400

    Documentation Update

--- Queries/ComplexQueries.sql
+++ Queries/ComplexQueries.sql
@@ -8,8 +8,10 @@
 SELECT OrdersDrug.HospIDNo, Drug.BrandName, OrdersDrug.Amount, (Drug.Cost*(1+(Stocks.MarkUp/100))*OrdersDrug.Amount) AS TotalCost
 FROM (Drug JOIN (Stocks JOIN OrdersDrug ON Stocks.DrugIDNo = OrdersDrug.DrugIDNo) ON (DrugID = Stocks.DrugIDNo AND Stocks.PharmIDNo = OrdersDrug.PharmIDNo))
 
--- Derived Attribute: Patient's Cost of Stay
+-- Derived Attribute: Patient's Total Days and Cost of Stay
 SELECT PatientIDNo, HospID, PrimaryDocID, DATEDIFF(day, Admitted, Discharged) AS Days, (DATEDIFF(day, Admitted, Discharged)*DailyCost) AS Cost
 FROM AdmittedTo, Hospital
 WHERE HospIDNo = HospID
 
+-- Derived Attribute: Nurse's NumPatients
+SELECT COUNT(*) FROM 
